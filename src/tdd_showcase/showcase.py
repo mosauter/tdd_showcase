e381c774d21907eadfa54c5553748d296d2479a3
Moritz Sauter <sautermoritz@gmx.de>
2022-05-03 13:58:08 +0200

    refactor: Final step

--- src/tdd_showcase/showcase.py
+++ src/tdd_showcase/showcase.py
@@ -3,13 +3,14 @@ from typing import Any, List
 
 
 def chunk(full_list: List[Any], chunks: int, chunk_index: int) -> range:
-    indices = range(len(full_list))
+    list_length = len(full_list)
+    indices = range(list_length)
 
-    chunk_size = ceil(len(full_list) / chunks)
+    chunk_size = ceil(list_length / chunks)
 
     chunks = [
         indices[index : index + chunk_size]
-        for index in range(0, len(full_list), chunk_size)
+        for index in range(0, list_length, chunk_size)
     ]
 
     return chunks[chunk_index]
