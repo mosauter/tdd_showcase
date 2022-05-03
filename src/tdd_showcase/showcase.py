31404459b5111dc7ce200b87c34a8e06fd71e1a2
Moritz Sauter <sautermoritz@gmx.de>
2022-05-03 13:51:13 +0200

    passing: second test passing

--- src/tdd_showcase/showcase.py
+++ src/tdd_showcase/showcase.py
@@ -1,5 +1,8 @@
+from math import ceil
 from typing import Any, List
 
 
 def chunk(full_list: List[Any], chunks: int, chunk_index: int) -> range:
-    return range(len(full_list))
+    chunk_size = ceil(len(full_list) / chunks)
+
+    return range(chunk_size)
